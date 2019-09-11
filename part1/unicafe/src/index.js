017b17daf85dff8491d3833c0e976093eeda424a
Fabio Jung <fabiolbj@gmail.com>
2019-09-11 19:44:32 -0400

    1.6: unicafe step1

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,15 +1,30 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Header = ({title}) => <h2>{title}</h2>
+
+const Button = ({text, onClick}) => <button onClick={onClick}>{text}</button>
+
+const Statistics = ({text, counter}) => <div>{text} {counter}</div>
+
 const App = () => {
   // save clicks of each button to own state
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const saveFeedback = (value, adder) => adder(value + 1)
+
   return (
     <div>
-      code here
+      <Header title="give feedback" />
+      <Button onClick={() => saveFeedback(good, setGood)} text="good" />
+      <Button onClick={() => saveFeedback(neutral, setNeutral)} text="neutral" />
+      <Button onClick={() => saveFeedback(bad, setBad)} text="bad" />
+      <Header title="statistics" />
+      <Statistics text="good" counter={good} />
+      <Statistics text="neutral" counter={neutral} />
+      <Statistics text="bad" counter={bad} />
     </div>
   )
 }
